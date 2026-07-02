8c06914c663778213e61befd244ebda122a246a0
JagadeeshHarshan <jagadeshharshan2@gmail.com>
2026-07-02 11:20:49 +0530

    first commit

---
+++ src/var_loop_condtions.ts
@@ -0,0 +1,84 @@
+// 1. Question
+// let score: number = 75;
+// if  (score >50) {
+//     console.log("pass");
+// }else{
+//     console.log("fail");
+// }
+
+// 2.question
+
+// let temperature: number = 30;
+// if (temperature > 30) {
+//     console.log("Hot");
+// }else {
+//     console.log("Normal")
+// }
+
+// 3.Question
+
+// let browser = "chrome"
+// switch (browser) {
+//     case browser:
+//         console.log("chrome");
+//         break;
+// }
+
+
+// 4. Question
+
+// let num: number = 20
+// for (let i = 1; i <=20 ; i++)
+// {
+// console.log(i);
+// }
+
+
+//5. question
+
+// let sum: number = (1+2+3+4+5+6+7+8+9+10);
+// console.log(sum);
+
+
+// 6. question
+// let sum: number = 0
+// for (let i = 1; i <=10; i++) {
+//     sum = sum +i
+// }
+// console.log(sum);
+
+// 7.Question
+
+// let num: number = 5;
+// for (let i =1; i<= 10; i++){
+//     console.log(`${num} * ${i} =${num * i}`);
+// }
+
+// 8. Question
+
+// let num: number = 2;
+// while(num <=20 
+// ){
+//     console.log(num);
+//     num += 2
+    
+// }
+
+// 9. Question
+ 
+// let num: number = 5; 
+// do {
+//     console.log(num);
+//     num--
+// } while (num >=1);
+
+// 10. question
+
+let num : number = 1;
+while (num <= -20
+
+){
+    console.log(num);
+    num--;
+}
+
