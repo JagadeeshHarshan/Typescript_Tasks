b1e9a9a73de75fd5c79b02150feb4aa79c720453
JagadeeshHarshan <jagadeshharshan2@gmail.com>
2026-07-02 13:00:33 +0530

    change

---
+++ src/varloopcondtions.ts
@@ -0,0 +1,91 @@
+//1. Question
+let score: number = 75;
+if  (score >50) {
+    console.log("pass");
+}else{
+    console.log("fail");
+}
+console.log ("score:" ,score);
+
+//2.question
+
+ let temperature: number = 35;
+if (temperature > 30) {
+    console.log("Hot");
+}else {
+    console.log("Normal")
+}
+console.log("temperature:" ,temperature);
+
+//3.Question
+
+let browser = "chrome"
+switch (browser) {
+    case browser:
+        console.log("chrome");
+        break;
+}
+console.log("browser:" ,browser);
+
+//4. Question
+
+ let num1: number = 20
+ for (let i = 1; i <=20 ; i++)
+ {
+ console.log(i);
+ }
+console.log("num:" , num1)
+
+
+//5. question
+
+let sum1: number = (1+2+3+4+5+6+7+8+9+10);
+console.log("sum:" , sum1);
+
+
+//6. question
+let sum2: number = 0
+for (let i = 1; i <=10; i++) {
+    sum2 = sum2 +i
+}
+console.log("sum:" , sum2);
+
+//7.Question
+
+let num2: number = 5;
+for (let i =1; i<= 10; i++){
+    console.log(`${num2} * ${i} =${num2 * i}`);
+}
+console.log("num:", num2)
+
+//8. Question
+
+let num3: number = 2;
+while(num3 <=20 
+){
+    console.log(num3);
+    num3 += 2
+    
+ }
+console.log("num:", num3);
+
+//9. Question
+ 
+let num: number = 5; 
+do {
+    console.log(num);
+    num--
+} while (num >=1);
+console.log("num:" , num);
+
+//10. question
+
+let numb : number = 1;
+while (numb >= -20
+
+){
+    console.log(numb);
+    numb--;
+}
+console.log("numb:", numb);
+
